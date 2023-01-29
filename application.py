a0b03450725f8d6b2ee88e1417fa79a53ad670a0
Sam Gulinello <sgulinello12@gmail.com>
2023-01-28 20:08:18 -0500

    get seed endpoint

--- application.py
+++ application.py
@@ -1,15 +1,34 @@
-import Flask
+from flask import Flask
+import yaml
 
-application = Flask()
+application = Flask(__name__)
+
+@application.route("/", methods = ['GET'])
+def helloWorld():
+
+    return 'HELLO WORLD!'
 
 @application.route("/getSeed", methods = ['GET'])
 def getSeed():
-    seed = 0
+    # read seed data from yml file
+    with open("seeds.yml") as file:
+        data = yaml.safe_load(file)
 
+    # get top seed value
+    seed = data[0]["seeds"][0]
+    data[0]["seeds"].remove(seed)
+
+    if(len(data[0]) <= 5):
+        pass
+        # GET MORE SEEDS
+
+    # update yml file
+    with open("seeds.yml", 'w') as file:
+        yaml.dump(data, file)
+    
     return seed
 
 
+application.run("0.0.0.0", port=5050)
 
 
-if("__name__" == "__main__"):
-    application.run("0.0.0.0")
